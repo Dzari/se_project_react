a562f47b2cc5092c02c5dd3c24b0af1a97111b97
Darien Sessions <darien.sessions@gmail.com>
2024-06-24 13:10:36 -0400

    adding and styling cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
+
 import './app.css';
 import Header from '../Header/header';
 import Main from '../Main/main';
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState({ type: 'cold' });
+
   return (
     <div className="app">
       <div className="app__wrapper">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,3 +1,10 @@
-export default function itemCard() {
-  return;
+import './itemCard.css';
+
+export default function itemCard({ card }) {
+  return (
+    <li className="card">
+      <h2 className="card__name">{card.name}</h2>
+      <img className="card__image" src={card.link} alt={card.name} />
+    </li>
+  );
 }
