96160248e31e1d6c2acd52a5cb2e2164e7cd6fac
Darien Sessions <darien.sessions@gmail.com>
2024-06-26 12:10:40 -0400

    updating file name on github

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,1 +1,24 @@
+import { weatherOptions } from '../../utils/constants';
+import './weatherCard.css';
 
+export default function weatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      weatherData.condition === option.condition &&
+      weatherData.isDay === option.day
+    );
+  });
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = '';
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F}&deg;F</p>
+      <img src={weatherOption.url} alt="" className="weather-card__image" />
+    </section>
+  );
+}
