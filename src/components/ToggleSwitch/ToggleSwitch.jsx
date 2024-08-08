c4654cad328afc147ea62c96718d821365a16ed2
Darien Sessions <darien.sessions@gmail.com>
2024-08-08 13:26:48 -0400

    added toggle for F to C

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import './toggleSwitch.css';
+import { CurrentTemperatureUnitContext } from '../../contexts/CurrentTempatureUnitContexts';
+
+const ToggleSwitch = () => {
+
+  const {currentTemperatureUnit, handleToggleSwitchChange} = useContext(CurrentTemperatureUnitContext)
+
+  return (
+    <label
+      className={currentTemperatureUnit === 'C' ? 'switch switch-C' : 'switch'}
+    >
+      <input type="checkbox" className="switch__box" onChange={handleToggleSwitchChange} />
+      <span className="switch__slider"></span>
+      <p
+        className={`switch__letter switch__unit-F
+            ${currentTemperatureUnit === 'C' ? 'switch__unit-inactive' : ''}
+          `}
+      >
+        F
+      </p>
+      <p
+        className={`switch__letter switch__unit-C
+          ${currentTemperatureUnit === 'F' ? 'switch__unit-inactive' : ''}
+        `}
+      >
+        C
+      </p>
+    </label>
+  );
+};
+
+export default ToggleSwitch;
