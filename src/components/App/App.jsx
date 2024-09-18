76f8faa2b8368419865f366512c3b8a9855efcf9
Darien Sessions <darien.sessions@gmail.com>
2024-09-18 16:04:43 -0400

    adding notes and cleaning up format

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+//***********************************Components ***********************************//
+
 import './app.css';
 import Header from '../Header/Header';
 import Main from '../Main/main.jsx';
 import Profile from '../Profile/Profile.jsx';
 import Footer from '../Footer/Footer';
+
+//*************************************Modals**************************************//
+
 import ItemModal from '../ItemModal/ItemModal.jsx';
-import { getWeather, filterWeatherData } from '../../utils/weatherAPI.js';
-import { APIKey, coordinates } from '../../utils/constants.js';
-import {
-  CurrentTemperatureUnitContext,
-  CurrentUserContext,
-  LoggedInContext,
-} from '../../contexts/contexts.js';
+import LoginModal from '../LoginModal/LoginModal.jsx';
 import AddItemModal from '../AddItemModal/AddItemModal.jsx';
+import SignupModal from '../SignupModal/SignupModal.jsx';
+import ProtectedRoute from '../ProtectedRoute/protectedRoute.jsx';
+import EditProfileModal from '../EditProfileModal/EditProfileModal.jsx';
+
+//*************************************API Calls***********************************//
+
+//Item API calls
 import {
   deleteItem,
   deleteLike,
@@ -22,31 +28,53 @@ import {
   likeItem,
   postItem,
 } from '../../utils/api.jsx';
-import LoginModal from '../LoginModal/LoginModal.jsx';
-import SignupModal from '../SignupModal/SignupModal.jsx';
-import ProtectedRoute from '../ProtectedRoute/protectedRoute.jsx';
+
+//User API calls
 import {
   getCurrentUser,
   login,
   signup,
   updateUser,
 } from '../../utils/auth.jsx';
-import EditProfileModal from '../EditProfileModal/EditProfileModal.jsx';
 
+//**********************************Imported Misc**********************************//
+
+import { APIKey, coordinates } from '../../utils/constants.js';
+import { getWeather, filterWeatherData } from '../../utils/weatherAPI.js';
+import {
+  CurrentTemperatureUnitContext,
+  CurrentUserContext,
+  LoggedInContext,
+} from '../../contexts/contexts.js';
+
+//*************************************App*****************************************//
 export default function App() {
+  //******************************Application States*******************************//
+
   const [weatherData, setWeatherData] = useState({
     type: '',
     temp: { F: 999, C: 999 },
     city: '',
   });
-  const token = localStorage.getItem('jwt');
+
+  //Determines which modal is visable
+
   const [activeModal, setActiveModal] = useState('');
   const [selectedCard, setSelectedCard] = useState({});
+
+  //Used to determine current temp & an array of clothing items which are used
+  // to filter based on temp
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState('F');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [clothingItems, setClothingItems] = useState([]);
+
+  //Sets which user is logged in and a boolean if user is guest or user
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState();
 
+  //Auth token used for authorization
+  const token = localStorage.getItem('jwt');
+
+  //Sets active modal
   const handleCardClick = (card) => {
     setActiveModal('preview');
     setSelectedCard(card);
@@ -68,48 +96,11 @@ export default function App() {
     setActiveModal('editProfile');
   };
 
-  const handleAddItemSubmit = (item) => {
-    postItem(item, token)
-      .then((item) => {
-        setClothingItems([item, ...clothingItems]);
-      })
-      .then(handleCloseModal)
-      .catch(console.error);
-  };
-
-  const handleDeleteItem = (deletedItem) => {
-    deleteItem(deletedItem, token)
-      .then(() => {
-        setClothingItems((clothingItems) =>
-          clothingItems.filter((item) => item._id !== deletedItem._id)
-        );
-      })
-      .then(handleCloseModal)
-      .catch(console.error);
-  };
-
-  const handleEditProfile = (data) => {
-    updateUser(data, token)
-      .then(getCurrentUser(token))
-      .then((user) => handleCurrentUser(user))
-      .catch((err) => console.log(err))
-      .finally(handleCloseModal);
-  };
-
   const handleCloseModal = () => {
     setActiveModal('');
   };
 
-  const handleToggleSwitchChange = () => {
-    currentTemperatureUnit === 'F'
-      ? setCurrentTemperatureUnit('C')
-      : setCurrentTemperatureUnit('F');
-  };
-
-  const handleIsLoggedIn = () => {
-    isLoggedIn === true ? setIsLoggedIn(false) : setIsLoggedIn(true);
-  };
-
+  //User click handlers
   const handleCardLike = (item) => {
     const id = item._id;
     if (item.likes.includes(currentUser._id)) {
@@ -131,12 +122,36 @@ export default function App() {
     }
   };
 
-  const handleCurrentUser = (user) => {
-    if (user) {
-      setIsLoggedIn(true);
-      setCurrentUser(user);
-    }
+  const handleDeleteItem = (deletedItem) => {
+    deleteItem(deletedItem, token)
+      .then(() => {
+        setClothingItems((clothingItems) =>
+          clothingItems.filter((item) => item._id !== deletedItem._id)
+        );
+      })
+      .then(handleCloseModal)
+      .catch(console.error);
   };
+
+  //Submit handlers
+  const handleAddItemSubmit = (item) => {
+    postItem(item, token)
+      .then((item) => {
+        setClothingItems([item, ...clothingItems]);
+      })
+      .then(handleCloseModal)
+      .catch(console.error);
+  };
+
+  const handleEditProfile = (data) => {
+    updateUser(data, token)
+      .then(getCurrentUser(token))
+      .then((user) => handleCurrentUser(user))
+      .catch((err) => console.log(err))
+      .finally(handleCloseModal);
+  };
+
+  //Authentication and sign in handlers
 
   const handleLogin = (data) => {
     login(data)
@@ -161,6 +176,30 @@ export default function App() {
       .catch((err) => console.log(err));
   };
 
+  //Changes contexts
+
+  const handleToggleSwitchChange = () => {
+    currentTemperatureUnit === 'F'
+      ? setCurrentTemperatureUnit('C')
+      : setCurrentTemperatureUnit('F');
+  };
+
+  const handleIsLoggedIn = () => {
+    isLoggedIn === true ? setIsLoggedIn(false) : setIsLoggedIn(true);
+  };
+
+  const handleCurrentUser = (user) => {
+    if (user) {
+      setIsLoggedIn(true);
+      setCurrentUser(user);
+    }
+  };
+
+
+  //*********************************Initial Functions*****************************//
+
+  //Pulls and sets weather data based on location 
+  // (location services still to be implemented)
   useEffect(() => {
     getWeather(coordinates, APIKey)
       .then((res) => {
@@ -170,6 +209,7 @@ export default function App() {
       .catch(console.error);
   }, []);
 
+  //Pulls all items and sets to clothingItems
   useEffect(() => {
     getItems()
       .then((data) => {
@@ -178,6 +218,8 @@ export default function App() {
       .catch(console.error);
   }, []);
 
+
+  //Pulls token if exists and sets auto logs in user
   useEffect(() => {
     if (!token) {
       return;
@@ -201,7 +243,6 @@ export default function App() {
                 handleLoginClick={handleLoginClick}
                 handleSignupClick={handleSignupClick}
               />
-
               <Routes>
                 <Route
                   path="/"
